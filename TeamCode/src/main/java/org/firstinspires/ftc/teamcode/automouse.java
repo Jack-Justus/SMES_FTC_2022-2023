e87dee87e991d4f81b8cc4786e8107f7683643f8
Andrew <andrewrichard1@outlook.com>
2022-10-04 15:35:11 -0700

    things

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automouse.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+public class automouse {
+
+    //go forward
+    //place thing on pole
+    //go back to starting position
+    //use computer vision to scan cone thing and identify were to park
+    //park in spot
+}

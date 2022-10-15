b1d5be5243146cebf5e928a7d41aae775dfbf5e2
Andrew <andrewrichard1@outlook.com>
2022-10-15 13:51:56 -0700

    things

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebcamTest.java
@@ -0,0 +1,111 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@TeleOp
+public class WebcamTest extends LinearOpMode {
+    OpenCvWebcam webcam;
+
+    @Override
+    public void runOpMode()
+    {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "PUT CAMERA CONFIG NAME HERE"), cameraMonitorViewId);
+        webcam.setPipeline(new SamplePipeline());
+
+        webcam.setMillisecondsPermissionTimeout(2500); // Timeout for obtaining permission is configurable. Set before opening.
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
+        telemetry.addLine("Waiting for start");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            /*
+             * Send some stats to the telemetry
+             */
+            telemetry.addData("Frame Count", webcam.getFrameCount());
+            telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
+            telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
+            telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
+            telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
+            telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
+            telemetry.update();
+
+            /*
+             * NOTE: stopping the stream from the camera early (before the end of the OpMode
+             * when it will be automatically stopped for you) *IS* supported. The "if" statement
+             * below will stop streaming from the camera when the "A" button on gamepad 1 is pressed.
+             */
+            if(gamepad1.a)
+            {
+                webcam.stopStreaming();
+                //webcam.closeCameraDevice();
+            }
+
+            sleep(100);
+        }
+    }
+
+    class SamplePipeline extends OpenCvPipeline
+    {
+        boolean viewportPaused;
+
+        /*
+         * NOTE: if you wish to use additional Mat objects in your processing pipeline, it is
+         * highly recommended to declare them here as instance variables and re-use them for
+         * each invocation of processFrame(), rather than declaring them as new local variables
+         * each time through processFrame(). This removes the danger of causing a memory leak
+         * by forgetting to call mat.release(), and it also reduces memory pressure by not
+         * constantly allocating and freeing large chunks of memory.
+         */
+
+        @Override
+        public Mat processFrame(Mat input)
+        {
+            //need to work on this
+            //this draw a rectangle, ingore this for now
+            Imgproc.rectangle(
+                    input,
+                    new Point(
+                            input.cols()/4,
+                            input.rows()/4),
+                    new Point(
+                            input.cols()*(3f/4f),
+                            input.rows()*(3f/4f)),
+                    new Scalar(0, 255, 0), 4);
+
+            return input;
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automouse.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automouse.java
@@ -1,10 +1,129 @@
 package org.firstinspires.ftc.teamcode;
 
-public class automouse {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
-    //go forward
-    //place thing on pole
-    //go back to starting position
-    //use computer vision to scan cone thing and identify were to park
-    //park in spot
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Autonomous", group = "Linear Opmode")
+public class automouse extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    // Left and Right Drive Motor Objects
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    //will be figured out in encoder test
+    private final float DIST_2_ENCODER = 0;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        //initialize motor things
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "lf");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "lb");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rb");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rf");
+
+        while (opModeIsActive()) {
+
+
+        }
+
+    }
+
+    private void move(int dir) {
+        /*
+        0 = forward
+        1 = right
+        2 = back
+        3 = left
+         */
+        int x;
+        int y;
+
+        switch (dir) {
+            case 0:
+                y = -1;
+                x = 0;
+                break;
+            case 1:
+                y = 0;
+                x = -1;
+                break;
+            case 2:
+                y = 1;
+                x = 0;
+                break;
+            case 3:
+                y = 0;
+                x = 1;
+                break;
+            default:
+                x = 0;
+                y = 0;
+                break;
+        }
+
+        leftFrontDrive.setPower(Range.clip(x + y, -1.0, 1.0));
+        leftBackDrive.setPower(Range.clip(y - x, -1.0, 1.0));
+
+        rightFrontDrive.setPower(-Range.clip(y - x, -1.0, 1.0));
+        rightBackDrive.setPower(Range.clip(x + y, -1.0, 1.0));
+
+    }
+
+    public void moveForward(int seconds) {
+
+        if (seconds == 0)
+            seconds = 1;
+
+        double baseline = runtime.milliseconds();
+
+        // Running until the amount of time has elapsed
+        while (runtime.milliseconds() < baseline + (seconds * 1000) && opModeIsActive())
+            move(0);
+    }
+
+    public void moveRight(int seconds) {
+
+        if (seconds == 0)
+            seconds = 1;
+
+        double baseline = runtime.milliseconds();
+
+        // Running until the amount of time has elapsed
+        while (runtime.milliseconds() < baseline + (seconds * 1000))
+            move(1);
+    }
+
+    public void moveBack(int seconds) {
+
+        if (seconds == 0)
+            seconds = 1;
+
+        double baseline = runtime.milliseconds();
+
+        // Running until the amount of time has elapsed
+        while (runtime.milliseconds() < baseline + (seconds * 1000))
+            move(2);
+    }
+
+    public void moveLeft(int seconds) {
+
+        if (seconds == 0)
+            seconds = 1;
+
+        double baseline = runtime.milliseconds();
+
+        // Running until the amount of time has elapsed
+        while (runtime.milliseconds() < baseline + (seconds * 1000))
+            move(3);
+    }
 }
